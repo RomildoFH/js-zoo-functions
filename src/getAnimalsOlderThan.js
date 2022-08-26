ed65dfe2d75dcfc979ca7ce65250c5f8d0808e4f
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-26 18:40:21 -0300

    Concluído requisito 2'

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,14 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
+const { species } = data;
 
 function getAnimalsOlderThan(animal, age) {
-
+  const especie = species.find((specie) => specie.name === animal);
+  // console.log(especie);
+  const residentes = especie['residents'];
+  // console.log(residentes);
+  return residentes.every((resident) => resident.age > age);
 }
+console.log(getAnimalsOlderThan('penguins', 18));
 
 module.exports = getAnimalsOlderThan;
