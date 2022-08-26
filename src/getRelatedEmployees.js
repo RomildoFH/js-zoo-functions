f63c194c0bf6ab00630ddd916266cb90541fb0b8
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-26 20:32:32 -0300

    Concluído requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -26,5 +26,5 @@ function getRelatedEmployees(managerId) {
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c099'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
