a212c15804a64b81db7bcf63b8d6f137561f9621
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-26 17:49:13 -0300

    Concluído requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui !!
-}
+const { species } = data;
+
+const getSpeciesByIds = (...ids) => {
+  // 1 - A função getSpeciesByIds, caso não receba nenhum parâmetro, deve retornar um array vazio;
+  const selected = [];
+  const idsLength = ids.length;
+  // 2 - A função getSpeciesByIds, caso receba como parâmetro um único ID, deve retornar um array com a espécie referente a esse ID;
+  if (idsLength !== 0) {
+    ids.forEach((id) => {
+      species.filter((element) => ((element.id === id) ? selected.push(element) : selected));
+    });
+  }
+  return selected;
+};
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
